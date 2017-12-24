772a1e1c6a96b002597c3ecd1e3988f0159add1b
Vzbiven <vzbiven@yandex.ru>
2017-12-24 23:05:58 +0300

    ship moves while key is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ def run_game():
     # Запуск основного цикла игры.
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()

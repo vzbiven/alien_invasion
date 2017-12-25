7ffe934703cac131b7070c10c7a9a9999435f88e
Vzbiven <vzbiven@yandex.ru>
2017-12-25 03:19:22 +0300

    deleting bullets from off-screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,13 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        # Удаление пуль вышедших за край экрана.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()

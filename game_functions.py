ca9b699227963201a6a5971f7cf92da6ab71825e
Vzbiven <vzbiven@yandex.ru>
2017-12-25 03:45:30 +0300

    added fire_bullets() [refactoring]

--- game_functions.py
+++ game_functions.py
@@ -12,10 +12,16 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #Создание новой пули и включение ее в группу bullets
-        if len(bullets) < ai_settings.bullets_allowed:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум еще не достигнут."""
+    # Создание новой пули и включение ее в в группу bullets
+    if len(bullets) < ai_settings.bullets_allowed:
             new_bullet = Bullet(ai_settings, screen, ship)
             bullets.add(new_bullet)
+
 
 def check_keyup_events(event, ship):
     """Реагирует на отпускание клавиш"""
@@ -47,6 +53,7 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     """Обновляет позиции пуль и удаляет старые пули."""

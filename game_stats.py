ca8ba452d8283a1fa40f8a95a9821d05cc2f5116
Vzbiven <vzbiven@yandex.ru>
2017-12-26 22:35:01 +0300

    record points reads and writes

--- game_stats.py
+++ game_stats.py
@@ -6,10 +6,15 @@ class GameStats():
         self.ai_settings = ai_settings
         self.reset_stats()
         self.game_active = False
-        self.high_score = 0
+        self.high_score = self.read_high_score()
 
     def reset_stats(self):
         """Инициализирует статистику, меняющуюся в ходе игры"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def read_high_score(self):
+        with open('save/record.txt', 'r') as f_obj:
+            hight_score = f_obj.read()
+        return int(hight_score)
